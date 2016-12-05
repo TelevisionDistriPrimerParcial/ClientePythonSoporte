6c90a9fe652f434af71e098013e7c2ab4716f099
ivan16luis <ivanlugiz16@yahoo.com>
2016-12-05 17:41:04 -0500

    conexion data java python funcionando

--- clientSupportPython/src/clientSupportPython.py
+++ clientSupportPython/src/clientSupportPython.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python
 import socket
 import sys
+import select
 
 HOST = '127.0.0.1'
 PORT = 9001
-s = None
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-    af, socktype, proto, canonname, sa = res
-    try:
-        s = socket.socket(af, socktype, proto)
-    except socket.error as msg:
-        s = None
-        continue
-    try:
-        s.connect(sa)
-    except socket.error as msg:
-        s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print 'could not open socket'
-    sys.exit(1)
-s.sendall('Hello, world')
-data = s.recv(1024)
-s.close()
-print 'Received', repr(data)
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.settimeout(2)
+try:
+    s.connect((HOST, PORT))
+except:
+    print "No se pudo conectar"
+    sys.exit()
+while True:
+    socket_list = [sys.stdin,s]
+    read_sockets, write_sockets, error_sockets = select.select(socket_list,[],[])
+    for sock in read_sockets:
+        if sock == s:
+            data = sock.recv(4096)
+            if not data:
+                print "\n Se a desconectado del servidor"
+                sys.exit()
+            else:
+                sys.stdout.write(data)
+                sys.stdout.write('[Support] '); sys.stdout.flush()
+        else:
+            msg = sys.stdin.readline()
+            s.send(msg)
+            sys.stdout.write('[Support] '); sys.stdout.flush()

a752029838700bee57d11ecb6e326b53fe6f865f
ivan16luis <ivanlugiz16@yahoo.com>
2016-12-09 14:23:29 -0500

    canal de comunicaciones con sokest python echo

--- clientSupportPython/src/clientSupportPython.py
+++ clientSupportPython/src/clientSupportPython.py
@@ -8,24 +8,41 @@ PORT = 9001
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.settimeout(2)
+
 try:
     s.connect((HOST, PORT))
 except:
     print "No se pudo conectar"
     sys.exit()
-while True:
-    socket_list = [sys.stdin,s]
-    read_sockets, write_sockets, error_sockets = select.select(socket_list,[],[])
-    for sock in read_sockets:
-        if sock == s:
-            data = sock.recv(4096)
-            if not data:
-                print "\n Se a desconectado del servidor"
-                sys.exit()
+
+sys.stdout.flush()
+
+def connect_server(mensaje):
+    data = ''
+    send_m = True
+    stop_b = True
+    while stop_b:
+        socket_list = [sys.stdin, s]
+        ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
+        for sock in ready_to_read:
+            if sock == s:
+                data = sock.recv(4096)
+                if not data:
+                    print '\nSe ha desconectado del Servidor'
+                    sys.exit()
+                else:
+                    data = data
+                    stop_b = False
             else:
-                sys.stdout.write(data)
-                sys.stdout.write('[Support] '); sys.stdout.flush()
-        else:
-            msg = sys.stdin.readline()
-            s.send(msg)
-            sys.stdout.write('[Support] '); sys.stdout.flush()
+                msg = mensaje
+                if send_m:
+                    s.send(msg)
+                    send_m = False
+
+    s.close()
+    return data
+
+print "Hacer una peticion: "
+msg = sys.stdin.readline()
+res = connect_server(msg)
+print res

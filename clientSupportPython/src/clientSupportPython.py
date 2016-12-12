4ce9b841e97af4e69e31d72b7ff4284eb6d3a767
ivan16luis <ivanlugiz16@yahoo.com>
2016-12-12 03:47:39 -0500

    listo login

--- clientSupportPython/src/clientSupportPython.py
+++ clientSupportPython/src/clientSupportPython.py
@@ -1,48 +1,24 @@
 #!/usr/bin/python
-import socket
-import sys
-import select
+import socket, sys
 
-HOST = '127.0.0.1'
-PORT = 9001
+HOST, PORT = "localhost", 9001
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.settimeout(2)
+def send_recived(data):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((HOST, PORT))
+        sub = 'SOPORT'
+        while True:
+            sock.sendall((bytes(sub + "\n")))
+            break
+        sock.sendall(bytes(data + "\n"))
+        received = str(sock.recv(4096))
+    finally:
+        sock.close()
+        return received
 
-try:
-    s.connect((HOST, PORT))
-except:
-    print "No se pudo conectar"
-    sys.exit()
+us = send_recived("RQSOPORT00000LOGIN020161203233018alias123|pass")
+print (us)
 
-sys.stdout.flush()
-
-def connect_server(mensaje):
-    data = ''
-    send_m = True
-    stop_b = True
-    while stop_b:
-        socket_list = [sys.stdin, s]
-        ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
-        for sock in ready_to_read:
-            if sock == s:
-                data = sock.recv(4096)
-                if not data:
-                    print '\nSe ha desconectado del Servidor'
-                    sys.exit()
-                else:
-                    data = data
-                    stop_b = False
-            else:
-                msg = mensaje
-                if send_m:
-                    s.send(msg)
-                    send_m = False
-
-    s.close()
-    return data
-
-print "Hacer una peticion: "
-msg = sys.stdin.readline()
-res = connect_server(msg)
-print res
+u = send_recived("RQSOPORT00000LOGIN020161203233018test_emp|123.456")
+print (u)
